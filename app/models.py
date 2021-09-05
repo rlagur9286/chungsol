f088f5abfd914621aaed9393a786f5dd92d0eab8
rlagur9286 <rlagur9286@naver.com>
2021-09-05 15:54:40 +0900

    2021-09-05, KH, 봄여름가을겨울

--- app/models.py
+++ app/models.py
@@ -2,8 +2,8 @@ from django.db import models
 class Image(models.Model):
     title = models.CharField(max_length=255)
     image = models.FileField(upload_to='static/img/')
-    desc1 = models.CharField(max_length=255, default='')
-    desc2 = models.CharField(max_length=255, default='')
+    desc1 = models.CharField(max_length=255, default='', blank=True)
+    desc2 = models.CharField(max_length=255, default='', blank=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import random
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
 from .models import Image
@@ -17,14 +18,61 @@ logger = logging.getLogger(__name__)
 
 
 def render_page(request):
-    main_img_ist = []
+
+    main_img_list = []
+    ###################################################
+    # MAIN 이미지
+    ###################################################
     for idx in range(1, 5):
         try:
             img = Image.objects.get(title="main{}".format(idx))
             img = model_to_dict(img)
         except ObjectDoesNotExist:
             img = None
-        main_img_ist.append(img)
+        main_img_list.append(img)
+
+    spring_img_list = []
+    summer_img_list = []
+    autumm_img_list = []
+    winter_img_list = []
+    ###################################################
+    # 계절별 이미지
+    ###################################################
+    try:
+        # 봄
+        images = Image.objects.filter(title__startswith="spring_")
+        for img in images:
+            img = model_to_dict(img)
+            spring_img_list.append(img)
+        random.shuffle(spring_img_list)
+
+        # 여름
+        images = Image.objects.filter(title__startswith="summer_")
+        for img in images:
+            img = model_to_dict(img)
+            summer_img_list.append(img)
+        random.shuffle(summer_img_list)
+
+        # 가을
+        images = Image.objects.filter(title__startswith="autumm")
+        for img in images:
+            img = model_to_dict(img)
+            autumm_img_list.append(img)
+        random.shuffle(autumm_img_list)
+
+        # 겨울
+        images = Image.objects.filter(title__startswith="winter_")
+        for img in images:
+            img = model_to_dict(img)
+            winter_img_list.append(img)
+        random.shuffle(winter_img_list)
+    except ObjectDoesNotExist:
+        spring_img_list, summer_img_list, autumm_img_list, winter_img_list = [], [], [], []
+
     return render(request, 'index.html', {
-        "main_img_ist": main_img_ist
+        "main_img_list": main_img_list,
+        "spring_img_list": spring_img_list,
+        "summer_img_list": summer_img_list,
+        "autumm_img_list": autumm_img_list,
+        "winter_img_list": winter_img_list,
     })
